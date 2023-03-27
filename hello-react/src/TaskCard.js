10570d9acf4f9ae8574ba2d7c1a37cf9f750a536
Devdeep Ghosh <devarmforce01@gmail.com>
2023-03-28 01:41:53 +0530

    hello=react commit

---
+++ hello-react/src/TaskCard.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "./TaskCard.css";
+
+/* interface Props {
+  title: string;
+  dueDate?: string;
+  completedAtDate?: string;
+  assigneeName: string;
+} */
+
+const TaskCard = (props) => {
+  return (
+    <div className="TaskItem">
+      <h2 className="text-xl font-bold">{props.title}</h2>
+      <p>{props.dueDate ? "Due on" : "Completed on"} : {props.dueDate ? props.dueDate : props.completedAtDate}</p>
+      <p>Assignee: {props.assigneeName}</p>
+    </div>
+  )
+}
+
+export default TaskCard;

ee8a60365c04861c3ec875999c3dd03e19ce63c2
Devdeep Ghosh <devarmforce01@gmail.com>
2023-04-04 16:41:49 +0530

    task card shows due on: and completed on:

--- hello-react/src/TaskCard.tsx
+++ hello-react/src/TaskCard.tsx
@@ -12,7 +12,7 @@ const TaskCard = (props: Props) => {
   return (
     <div className="TaskItem">
       <h2 className="text-xl font-bold">{props.title}</h2>
-      <p>{props.dueDate ? "Due on" : "Completed on"} : {props.dueDate ? props.dueDate : props.completedAtDate}</p>
+      <p>{props.dueDate ? "Due on:" : "Completed on:"} {props.dueDate ? props.dueDate : props.completedAtDate}</p>
       <p>Assignee: {props.assigneeName}</p>
     </div>
   )

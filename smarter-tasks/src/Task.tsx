9223825e739cc80e7de5fb79b299b7ea8bb50aae
Devdeep Ghosh <devarmforce01@gmail.com>
2023-05-10 23:43:38 +0530

    li tag fix

--- smarter-tasks/src/Task.tsx
+++ smarter-tasks/src/Task.tsx
@@ -11,21 +11,23 @@ interface TaskProp {
 
 const Task = (props: TaskProp) => {
   return (
-    <div className="TaskItem flex items-center justify-between shadow-md border border-slate-100">
-      <li>
+    <li className="TaskItem flex items-center justify-between shadow-md border border-slate-100">
+      <div>
         <h2 className="text-base font-bold my-1">{props.title}</h2>
         <p className="text-sm text-slate-500">{props.dueDate}</p>
         <p className="text-sm text-slate-500">
           Description: {props.description}
         </p>
-        <button
-          className="deleteTaskButton p-2 bg-blue-400 text-white mr-5"
-          onClick={() => props.deleteTask(props.id)}
-        >
-          Delete
-        </button>
-      </li>
-    </div>
+      </div>
+      <button
+        className="deleteTaskButton p-2 bg-blue-400 text-white mr-5"
+        onClick={
+          () => props.deleteTask(props.id)
+        }
+      >
+        Delete
+      </button>
+    </li>
   );
 };
 

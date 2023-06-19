9310f08423bb3d31d58e430aac077f47f583e8f6
Devdeep Ghosh <devarmforce01@gmail.com>
2023-06-19 12:01:13 +0530

    smarter-tasks added header signin and signout andf protected routes

--- smarter-tasks/src/App.tsx
+++ smarter-tasks/src/App.tsx
@@ -1,15 +1,29 @@
 import HomePage from "./HomePage";
 import TaskApp from "./TaskApp";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useLocation } from "react-router-dom";
 import TaskDetailsPage from "./TaskDetailsPage";
+import Header from "./Header";
+import Signin from "./Signin";
+import { ProtectedRoute } from "./ProtectedRoute";
 
 function App() {
+  const location = useLocation();
   return (
-    <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/tasks" element={<TaskApp />} />
-      <Route path="/tasks/:id" element={<TaskDetailsPage />} />
-    </Routes>
+    <div>
+      {location.pathname !== "/signin" && <Header />}
+      <Routes>
+        <Route path="/" element={<ProtectedRoute element={<HomePage />} />} />
+        <Route
+          path="/tasks"
+          element={<ProtectedRoute element={<TaskApp />} />}
+        />
+        <Route
+          path="/tasks/:id"
+          element={<ProtectedRoute element={<TaskDetailsPage />} />}
+        />
+        <Route path="/signin" element={<Signin />} />
+      </Routes>
+    </div>
   );
 }
 

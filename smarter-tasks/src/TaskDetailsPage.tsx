fc3a18b7836346ac07860a24c2f4cd732775160c
Devdeep Ghosh <devarmforce01@gmail.com>
2023-07-07 01:07:44 +0530

    tasksdetail page shows task title

--- smarter-tasks/src/TaskDetailsPage.tsx
+++ smarter-tasks/src/TaskDetailsPage.tsx
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import { TaskItem } from "./types";
 
 interface TaskDetailsPageParams extends Record<string, string> {
   id: string;
@@ -7,9 +8,25 @@ interface TaskDetailsPageParams extends Record<string, string> {
 
 const TaskDetailsPage: React.FC = () => {
   const { id } = useParams<TaskDetailsPageParams>();
+  const [task, setTask] = useState<TaskItem>({
+    id: 0,
+    title: "",
+    dueDate: "",
+    description: "",
+  });
+
+  useEffect(() => {
+    const localtask = localStorage.getItem("tasks");
+    const parsedTasks: TaskItem[] = localtask && JSON.parse(localtask).tasks;
+    const f_task: TaskItem | undefined = parsedTasks.find(
+      (task) => task.id === Number(id)
+    );
+    f_task && setTask(f_task);
+  }, [id, task]);
+
   return (
     <div>
-      <h1>Task Details</h1>
+      <h3>{task.title}</h3>
       <p>This is the Task Details page for task with ID: {id}</p>
     </div>
   );

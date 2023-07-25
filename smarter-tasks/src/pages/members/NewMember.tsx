ceae1c119aa1c6959f989a3e4e4f46c7321955a9
Devdeep Ghosh <devarmforce01@gmail.com>
2023-07-25 16:17:18 +0530

    add member

--- smarter-tasks/src/pages/members/NewMember.tsx
+++ smarter-tasks/src/pages/members/NewMember.tsx
@@ -1,16 +1,17 @@
 import { Dialog, Transition } from "@headlessui/react";
 import { Fragment, useState } from "react";
 import { useForm, SubmitHandler } from "react-hook-form";
-import { addMember } from "../../context/members/actions";
 import { useMembersDispatch } from "../../context/members/context";
+import { addMember } from "../../context/members/actions";
+
 type Inputs = {
   name: string;
   email: string;
   password: string;
 };
+
 const NewMember = () => {
   const [isOpen, setIsOpen] = useState(false);
-
   const [error, setError] = useState(null);
   const dispatchMembers = useMembersDispatch();
   const {
@@ -26,13 +27,13 @@ const NewMember = () => {
   };
   const onSubmit: SubmitHandler<Inputs> = async (data) => {
     const { name, email, password } = data;
+
     const response = await addMember(dispatchMembers, {
       name,
       email,
       password,
     });
 
-    // Then depending on response, I'll either close the modal...
     if (response.ok) {
       setIsOpen(false);
     } else {
@@ -78,15 +79,16 @@ const NewMember = () => {
                     as="h3"
                     className="text-lg font-medium leading-6 text-gray-900"
                   >
-                    Create new member
+                    Add new member
                   </Dialog.Title>
                   <div className="mt-2">
                     <form onSubmit={handleSubmit(onSubmit)}>
+                      {/* I'll show the error, if it exists.*/}
                       {error && <span>{error}</span>}
                       <input
                         type="text"
                         id="name"
-                        placeholder="Enter member name..."
+                        placeholder="Enter user name..."
                         autoFocus
                         {...register("name", { required: true })}
                         className={`w-full border rounded-md py-2 px-3 my-4 text-gray-700 leading-tight focus:outline-none focus:border-blue-500 focus:shadow-outline-blue ${
@@ -97,8 +99,7 @@ const NewMember = () => {
                       <input
                         type="email"
                         id="email"
-                        placeholder="Enter member email..."
-                        autoFocus
+                        placeholder="Enter email..."
                         {...register("email", { required: true })}
                         className={`w-full border rounded-md py-2 px-3 my-4 text-gray-700 leading-tight focus:outline-none focus:border-blue-500 focus:shadow-outline-blue ${
                           errors.email ? "border-red-500" : ""
@@ -108,8 +109,7 @@ const NewMember = () => {
                       <input
                         type="password"
                         id="password"
-                        placeholder="Enter member password..."
-                        autoFocus
+                        placeholder="Enter password..."
                         {...register("password", { required: true })}
                         className={`w-full border rounded-md py-2 px-3 my-4 text-gray-700 leading-tight focus:outline-none focus:border-blue-500 focus:shadow-outline-blue ${
                           errors.password ? "border-red-500" : ""
@@ -118,12 +118,13 @@ const NewMember = () => {
                       {errors.password && <span>This field is required</span>}
                       <button
                         type="submit"
+                        id="create-member-btn"
                         className="inline-flex justify-center rounded-md border border-transparent bg-blue-600 px-4 py-2 mr-2 text-sm font-medium text-white hover:bg-blue-500 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
                       >
                         Submit
                       </button>
                       <button
-                        type="submit"
+                        type="button"
                         onClick={closeModal}
                         className="inline-flex justify-center rounded-md border border-transparent bg-blue-100 px-4 py-2 text-sm font-medium text-blue-900 hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
                       >

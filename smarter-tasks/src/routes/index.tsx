001b8b14a0808c55d6c6090eab8e757787903bff
Devdeep Ghosh <devarmforce01@gmail.com>
2023-07-25 16:01:49 +0530

    not found added in src

--- smarter-tasks/src/routes/index.tsx
+++ smarter-tasks/src/routes/index.tsx
@@ -7,7 +7,7 @@ import Signup from "../pages/signup";
 import Projects from "../pages/projects";
 import Members from "../pages/members";
 import Logout from "../pages/logout";
-import NotFound from "../pages/notfound";
+import NotFound from "../NotFound";
 
 const router = createBrowserRouter([
   { path: "/", element: <Navigate to="/account/projects" replace /> },

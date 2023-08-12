01719f72d91e045f002eaec5815ba1f23ae8e440
Devdeep Ghosh <devarmforce01@gmail.com>
2023-08-12 15:40:32 +0530

    build bug

--- smarter-tasks/vite.config.ts
+++ smarter-tasks/vite.config.ts
@@ -1,7 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-});
+export default ({ mode }) => {
+  return defineConfig({
+    build: {
+      outDir: 'build',
+    },
+    plugins: [react()],
+    define: {
+      "process.env.NODE_ENV": `"${mode}"`,
+    },
+  });
+};

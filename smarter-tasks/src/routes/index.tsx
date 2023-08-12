aa234354037928cc3110e27a3dedd9c218d602c2
Devdeep Ghosh <devarmforce01@gmail.com>
2023-08-12 16:00:41 +0530

    notfound fix

--- smarter-tasks/src/routes/index.tsx
+++ smarter-tasks/src/routes/index.tsx
@@ -7,7 +7,7 @@ import Signup from "../pages/signup";
 import Projects from "../pages/projects";
 import Members from "../pages/members";
 import Logout from "../pages/logout";
-import NotFound from "../pages/NotFound";
+import NotFound from "../pages/Notfound";
 import ProjectContainer from "../pages/projects/ProjectContainer";
 import ProjectDetails from "../pages/project_details";
 import NewTask from "../pages/tasks/NewTask";
